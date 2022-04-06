705efc64f70b4a0096f601ddaee75373f87caca2
Matjhitje Mokate <matjhitjemokate@gmail.com>
2022-04-06 11:51:02 +0200

    Added Skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AcademicsComponent } from './academics/academics.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcademicsComponent
+    AcademicsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
