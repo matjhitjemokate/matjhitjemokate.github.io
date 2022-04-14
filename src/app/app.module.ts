10c651edbef378263f3b95f67d8b2bb4afc281bb
Matjhitje Mokate <matjhitjemokate@gmail.com>
2022-04-14 22:00:44 +0200

    null

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,15 +16,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatGridListModule } from '@angular/material/grid-list';
-
 import { MatCardModule } from '@angular/material/card';
-
 import { MatExpansionModule } from '@angular/material/expansion';
-
 import { MatDividerModule } from '@angular/material/divider';
-
 import { MatTabsModule } from '@angular/material/tabs';
-
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
